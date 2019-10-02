2ccc0c8cefe61e74cd56676c26283129cf4823bd
Quoc Khanh <nqkhanh.1110@gmail.com>
2019-10-03 01:04:32 +0700

    add Navigation Drawer

--- app/src/main/java/com/quockhanhng/training/messenger/activity/MainActivity.kt
+++ app/src/main/java/com/quockhanhng/training/messenger/activity/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.quockhanhng.training.messenger.activity
 
 import android.content.Intent
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
-import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.*
@@ -20,6 +23,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var rootLayout: DrawerLayout
+    private lateinit var drawerToggle: ActionBarDrawerToggle
     private lateinit var auth: FirebaseAuth
     private var user: FirebaseUser? = null
     private lateinit var myUser: User
@@ -33,16 +38,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        rootLayout = findViewById(R.id.rootLayout)
+        drawerToggle =
+            ActionBarDrawerToggle(this, rootLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        rootLayout.addDrawerListener(drawerToggle)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_drawable)
+
+        drawerToggle.syncState()
+
+        navigation_menu.setNavigationItemSelectedListener(object : NavigationView.OnNavigationItemSelectedListener {
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                when (item.itemId) {
+                    R.id.mi_account -> goToProfileAccount()
+                    R.id.mi_settings -> goToSettings()
+                    R.id.mi_logout -> logOut()
+                    else -> return true
+                }
+
+                return true
+            }
+        })
+
         auth = FirebaseAuth.getInstance()
         user = auth.currentUser
 
-        //Check if user has signed in before else redirect to login page
+        // Check if user has signed in before else redirect to login page
         if (user == null) {
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
             return
         }
 
+        setUpData()
+    }
+
+    private fun setUpData() {
         userId = user?.uid.toString()
         database = FirebaseFirestore.getInstance()
 
@@ -61,23 +94,20 @@ class MainActivity : AppCompatActivity() {
 
         adapter = MessageAdapter(query, userId, this@MainActivity)
         recyclerViewList.adapter = adapter
+        recyclerViewList.smoothScrollToPosition(adapter?.itemCount!! + 1)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu, menu)
-        return true
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        drawerToggle.onConfigurationChanged(newConfig)
     }
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.menu_sign_out -> {
-                auth.signOut()
-                finish()
-                startActivity(Intent(this, LoginActivity::class.java))
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
+        if (drawerToggle.onOptionsItemSelected(item)) return true
+
+        return super.onOptionsItemSelected(item)
     }
 
     fun sendMessage(v: View) {
@@ -89,6 +119,7 @@ class MainActivity : AppCompatActivity() {
         database.collection("messages").add(Message(userName, message, userId, 0, null))
         adapter?.notifyDataSetChanged()
         input.setText("")
+        recyclerViewList.smoothScrollToPosition(adapter?.itemCount!! + 1)
     }
 
     override fun onStart() {
@@ -101,5 +132,19 @@ class MainActivity : AppCompatActivity() {
         super.onStop()
 
         adapter?.stopListening()
+    }
+
+    private fun goToProfileAccount() {
+        Toast.makeText(this, "Profile Account", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun goToSettings() {
+        Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun logOut() {
+        auth.signOut()
+        finish()
+        startActivity(Intent(this, LoginActivity::class.java))
     }
 }
